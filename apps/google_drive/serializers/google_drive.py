64de508bc4884e1c5f061ee3201d6ca42b07e495
Vitaly Stain <vitalikstain@inbox.ru>
2023-11-15 14:57:25 +0300

    chore: turn off celery task

--- apps/google_drive/serializers/google_drive.py
+++ apps/google_drive/serializers/google_drive.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
-from apps.google_drive.tasks import create_file_in_google_drive
+from apps.google_drive.services.google_drive import GoogleDriveService
+
+
+# from apps.google_drive.tasks import create_file_in_google_drive
 
 
 class GoogleDriveDocCreateSerializer(serializers.Serializer):
@@ -8,7 +11,13 @@ class GoogleDriveDocCreateSerializer(serializers.Serializer):
     data = serializers.CharField(max_length=None)
 
     def create(self, validated_data):
-        create_file_in_google_drive.apply_async(
-            args=[validated_data["name"], validated_data["data"]]
-        )
+        # use celery if deploy services allow it
+
+        # create_file_in_google_drive.apply_async(
+        #     args=[validated_data["name"], validated_data["data"]]
+        # )
+
+        service = GoogleDriveService()
+        service.create_text_file(validated_data["name"], validated_data["data"])
+
         return validated_data
